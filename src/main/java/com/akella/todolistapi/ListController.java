0d2faf07821a3021574afef0797ab1e9e5c3cc13
Akella55505 <leosaz0617@gmail.com>
2025-09-07 14:23:40 +0300

    Created a test mapping

--- src/main/java/com/akella/todolistapi/ListController.java
+++ src/main/java/com/akella/todolistapi/ListController.java
@@ -1,4 +1,15 @@
 package com.akella.todolistapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class ListController {
+
+    @GetMapping("/test")
+    public String returnString() {
+        return "This is a test string";
+    }
 }
--- src/main/java/com/akella/todolistapi/ToDoListApiApplication.java
+++ src/main/java/com/akella/todolistapi/ToDoListApiApplication.java
@@ -2,12 +2,21 @@ package com.akella.todolistapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication()
+@RestController
 public class ToDoListApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApiApplication.class, args);
-	}
+    @GetMapping("/hi")
+    public String testMethod()
+    {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApiApplication.class, args);
+    }
 
 }
