d2b71c99f12a3326bd9c45c7aaf5f58c719a0b81
Akella <leosaz0617@gmail.com>
2025-09-09 15:33:01 +0300

    v1 finished

---
+++ src/main/java/com/akella/todolistapi/Task.java
@@ -0,0 +1,23 @@
+package com.akella.todolistapi;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="Tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private LocalDateTime deadlineDateTime;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private Boolean isCompleted = false;
+}
---
+++ src/main/java/com/akella/todolistapi/TaskController.java
@@ -0,0 +1,43 @@
+package com.akella.todolistapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/tasks")
+    public ResponseEntity<?> saveTask(@RequestBody Task task) {
+        try {
+            taskRepository.save(task);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PatchMapping("/tasks/{id}")
+    public Task completeTask(@PathVariable("id") Long id) {
+        Task task = taskRepository.getReferenceById(id);
+        task.setIsCompleted(Boolean.TRUE);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable("id") Long id) {
+        taskRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
