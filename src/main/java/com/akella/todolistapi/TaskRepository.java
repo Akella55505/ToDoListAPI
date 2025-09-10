362536fbc2df86db92e666ae0be8f2c07fc1c7ef
Akella <leosaz0617@gmail.com>
2025-09-10 11:12:23 +0300

    Added annotation

--- src/main/java/com/akella/todolistapi/TaskRepository.java
+++ src/main/java/com/akella/todolistapi/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.akella.todolistapi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
