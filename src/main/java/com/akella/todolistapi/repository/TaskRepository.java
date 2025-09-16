ee205ef98168aaa90e52467b28c8f9f82d90a7de
Akella <leosaz0617@gmail.com>
2025-09-17 00:41:35 +0300

    User-specific login implemented

--- src/main/java/com/akella/todolistapi/repository/TaskRepository.java
+++ src/main/java/com/akella/todolistapi/repository/TaskRepository.java
@@ -1,9 +1,17 @@
 package com.akella.todolistapi.repository;
 
 import com.akella.todolistapi.model.Task;
+import com.akella.todolistapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUserOrderByDeadlineDateTimeAsc(User user);
+
+    Optional<Task> findByIdAndUser(Long id, User user);
 }
